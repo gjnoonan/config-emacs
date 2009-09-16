fd124fbe36d82477c13166345de60d2eee8f459f
Gavin-John Noonan <gavinjohn.noonan@me.com>
2009-09-16 14:48:19 +0100

    A somewhat slimmed down version of the emacs-starter-kit init.el, I have removed things to do with the ELPA since I won't be using it (yet??)

---
+++ init.el
@@ -0,0 +1,35 @@
+(setq dotfiles-dir (file-name-directory
+                    (or (buffer-file-name) load-file-name)))
+
+(add-to-list 'load-path dotfiles-dir)
+
+(setq autoload-file (concat dotfiles-dir "loaddefs.el"))
+(setq custom-file (concat dotfiles-dir "custom.el"))
+
+;; These should be loaded on startup rather than autoloaded on demand
+;; since they are likely to be used in every session
+
+(require 'cl)
+(require 'saveplace)
+(require 'ffap)
+(require 'uniquify)
+(require 'ansi-color)
+(require 'recentf)
+
+(regen-autoloads)
+(load custom-file 'noerror)
+
+;; Work around a bug on OS X where system-name is FQDN
+(if (eq system-type 'darwin)
+    (setq system-name (car (split-string system-name "\\."))))
+
+;; You can keep system- or user-specific customizations here
+(setq system-specific-config (concat dotfiles-dir system-name ".el")
+      user-specific-config (concat dotfiles-dir user-login-name ".el")
+      user-specific-dir (concat dotfiles-dir user-login-name))
+(add-to-list 'load-path user-specific-dir)
+
+(if (file-exists-p system-specific-config) (load system-specific-config))
+(if (file-exists-p user-specific-config) (load user-specific-config))
+(if (file-exists-p user-specific-dir)
+  (mapc #'load (directory-files user-specific-dir nil ".*el$")))
