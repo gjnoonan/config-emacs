39535ad17cc1ca1c7534932125e30c5575676698
Gavin-John Noonan <gavinjohn.noonan@me.com>
2009-09-16 15:09:49 +0100

    Default appearance and color-theme

--- adulteratedjedi.el
+++ adulteratedjedi.el
@@ -1,2 +1,3 @@
 (load "adulteratedjedi/global")
 (load "adulteratedjedi/defuns")
+(load "adulteratedjedi/appearance")
---
+++ adulteratedjedi/appearance.el
@@ -0,0 +1,6 @@
+(set-face-font 'default "-apple-inconsolata-medium-r-normal--20-0-72-72-m-0-iso10646-1")
+
+;; Color Themes
+(vendor 'color-theme)
+
+(setq color-theme-is-global t)
